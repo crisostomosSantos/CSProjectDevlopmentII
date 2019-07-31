296fbb8c3218eb209ecbcfdfe01000235c73992c
Cris <Cris@10.0.0.105>
2019-07-31 19:33:07 -0300

    Commit

---
+++ src/main/java/com/csproject/crudrest/api/CrudrestFullApplication.java
@@ -0,0 +1,13 @@
+package com.csproject.crudrest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudrestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudrestFullApplication.class, args);
+	}
+
+}
